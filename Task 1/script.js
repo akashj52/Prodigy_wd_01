e05840ed5e1c8bbef6d33b640ce12648735cbde4
akashj52 <akashj150411@gmail.com>
2025-09-20 19:43:03 +0530

    Responsive Landing Page

---
+++ Task 1/script.js
@@ -0,0 +1,11 @@
+// Select navbar
+const navbar = document.getElementById("navbar");
+
+// Add scroll event listener
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
